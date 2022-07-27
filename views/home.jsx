198da1807d0cbb56ff27fd1cbca6a4c6fc456cfb
Marceilla Norman <101609514+Spunkytanadi@users.noreply.github.com>
2022-07-27 16:57:15 -0500

    Adding places view with mock data

--- views/home.jsx
+++ views/home.jsx
@@ -2,13 +2,13 @@ const React = require('react')
 const Def = require('./default')
 
 function home () {
-    return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
-    )
-  }  
+  return (
+    <Def>
+        <main>
+            <h1>HOME</h1>
+        </main>
+    </Def>
+  )
+}
 
 module.exports = home
