b12a6c1f99718d0c9f9a056ccb4447b8c92c7f47
Marceilla Norman <101609514+Spunkytanadi@users.noreply.github.com>
2022-08-01 20:28:28 -0500

    Delete function Implemented

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
-app.use(express.urlencoded({ extended: true }))
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
 
 app.use('/places', require('./controllers/places'))
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,7 +6,15 @@ function show (data) {
         <Def>
           <main>
             <h1>{ data.place.name }</h1>
-          </main>
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                    Edit
+                </a>  
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                    <button type="submit" className="btn btn-danger">
+                     Delete
+                    </button>
+                 </form> 
+          </main>    
         </Def>
     )
 }
