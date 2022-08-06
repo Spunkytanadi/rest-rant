b5cfd119e2603dc42f3272034847c29bb528abc3
Marceilla Norman <101609514+Spunkytanadi@users.noreply.github.com>
2022-08-05 19:05:18 -0500

    Implementing the mongoose ODM

--- controllers/places.js
+++ controllers/places.js
@@ -2,27 +2,26 @@ const router = require('express').Router()
 const db = require('../models')
 
 router.get('/', (req, res) => {
-    db.Place.find()
-    .then((places) => {
-      res.render('places/index', { places })
-    })
-    .catch(err => {
-      console.log(err) 
-      res.render('error404')
-    })
+  db.Plave.find()
+  .then((places) => {
+    res.render('places/index', {places})
+  })
+  .catch(err => {
+    console.log(err)
+    res.render('error404')
+  })
 })
 
 router.post('/', (req, res) => {
   db.Place.create(req.body)
   .then(() => {
-      res.redirect('/places')
+    res.redirect('/places')
   })
   .catch(err => {
-      console.log('err', err)
-      res.render('error404')
+    console.log('err', err)
+    res.render('error404')
   })
 })
-
 
 router.get('/new', (req, res) => {
   res.render('places/new')
@@ -31,14 +30,13 @@ router.get('/new', (req, res) => {
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
   .then(place => {
-      res.render('places/show', { place })
+    res.render('places/show', {place})
   })
   .catch(err => {
-      console.log('err', err)
-      res.render('error404')
+    console.log('err', err)
+    res.render('error404')
   })
 })
-
 
 router.put('/:id', (req, res) => {
   res.send('PUT /places/:id stub')
@@ -59,46 +57,5 @@ router.post('/:id/rant', (req, res) => {
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
 })
-
-router.post('/', (req, res) => {
-  console.log(req.body)
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
-})
-
-router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id], id })
-  }
-})
-
-router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.redirect('/places')
-  }
-})
-
 
 module.exports = router
