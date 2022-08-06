eec42a9f9bb1f441b87fafed23516ff4fdd4ff0f
Marceilla Norman <101609514+Spunkytanadi@users.noreply.github.com>
2022-08-06 13:15:50 -0500

    Update second method

--- controllers/places.js
+++ controllers/places.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const db = require('../models')
 
 router.get('/', (req, res) => {
-  db.Plave.find()
+  db.Place.find()
   .then((places) => {
     res.render('places/index', {places})
   })
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,13 +6,9 @@ function show (data) {
         <Def>
           <main>
             <h1>{ data.place.name }</h1>
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                    Edit
-                </a>  
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>  
                 <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                    <button type="submit" className="btn btn-danger">
-                     Delete
-                    </button>
+                    <button type="submit" className="btn btn-danger">Delete</button>
                  </form> 
           </main>    
         </Def>
